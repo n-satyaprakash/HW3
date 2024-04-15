1168b5e22f381fa37bcbf151893d4dc0d858bbb4
n-satyaprakash <snandigam@my.okcu.edu>
2024-04-15 17:56:09 -0500

    finalcommit

--- js/Dictionary.js
+++ js/Dictionary.js
@@ -1,16 +1,21 @@
 document.getElementById('searchButton').addEventListener('click', function() {
     var query = document.getElementById('searchQuery').value;
-    if(query) {
+    if (query) {
         fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + query)
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.json();
+            })
             .then(data => {
                 var meanings = data[0].meanings.map(function(meaning) {
                     return '<li>' + meaning.definitions[0].definition + '</li>';
                 }).join('');
-                document.getElementById('searchResults').innerHTML = '<ul>' + meanings + '</ul>';
+                document.getElementById('searchResults').innerHTML = '<ol>' + meanings + '</ol>';
             })
             .catch(error => {
-                document.getElementById('searchResults').innerHTML = '<p>Error fetching results.</p>';
+                document.getElementById('searchResults').innerHTML = '<p>Error fetching results: ' + error.message + '</p>';
             });
     }
 });
